0cebb7a20486e188ef3c9a51003356e09a08ac61
Private <jgomezz@gmail.com>
2019-05-16 11:52:04 -0500

    Update PetService2Test with findByName method

--- src/test/java/com/tecsup/petclinic/service/PetService2Test.java
+++ src/test/java/com/tecsup/petclinic/service/PetService2Test.java
@@ -1,13 +1,18 @@
 package com.tecsup.petclinic.service;
 
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
 import com.tecsup.petclinic.domain.Pet;
 
 @SpringBootTest
@@ -15,17 +20,32 @@ import com.tecsup.petclinic.domain.Pet;
 @AutoConfigureTestDatabase(replace = Replace.NONE)
 public class PetService2Test {
 
+	 
+	private static final Logger logger = LoggerFactory.getLogger(PetService2Test.class);
+
+	
 	@Autowired
 	private PetService petService;
 	
-
 	@Test
-	public void testFindById() {
-		
+	public void testFindById() {		
 		// Sly		
 		long id = 13;
 		Pet p = petService.findById(id);
 		Assert.assertEquals("Sly", p.getName());
+	}
+	
+	@Test
+	public void testFindByName() {
+		
+		String NAME = "Iggy";
+		int SIZE_EXPECTED = 1;
+		
+		List<Pet> pets = petService.findByName(NAME);
+		
+		pets.stream().forEach(pet ->logger.info(">>>>>>>" +pet.getId()));
+		
+		Assert.assertEquals(SIZE_EXPECTED, pets.size());
 		
 	}
 }
